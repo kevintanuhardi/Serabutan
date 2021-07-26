3bff087cc41bac7b7b675c15d923559e5b2fc2dd
Gilang Adrian <gilang.adrian@outlook.com>
2021-07-27 00:36:42 +0700

    Gilang's branch
    make new branch and layout

---
+++ Serabutan/Features/View/ImageCarouselCVC.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCarouselCVC.swift
+//  Serabutan
+//
+//  Created by Gilang Adrian on 26/07/21.
+//
+
+import UIKit
+
+class ImageCarouselCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
