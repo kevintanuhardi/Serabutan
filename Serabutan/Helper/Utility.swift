6ea2edac2af53207600c7981961b49e0548a4ab6
kevintanuhardi <42530435+kevintanuhardi@users.noreply.github.com>
2021-08-03 10:37:55 +0700

    Revert "Revert "Feat/home""

---
+++ Serabutan/Helper/Utility.swift
@@ -0,0 +1,21 @@
+//
+//  Utility.swift
+//  Serabutan
+//
+//  Created by Kevin Tanuhardi on 25/07/21.
+//
+
+import Foundation
+
+extension Formatter {
+    static let withSeparator: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.groupingSeparator = ","
+        return formatter
+    }()
+}
+
+extension Numeric {
+    var formattedWithSeparator: String { Formatter.withSeparator.string(for: self) ?? "" }
+  }
